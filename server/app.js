b6fb558a00d9a894b220ac4f39cef9e4d3e4653c
Anmol <anmolgaur26@gmail.com>
2023-09-29 21:55:09 +0530

    Login and  lregister endpoint done endpoints done

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const bycrypt=require('bcryptjs')
+const bycrypt = require('bcryptjs');
+const Users = require('./models/Users');
+const jwt = require('jsonwebtoken')
+
 require('./db/connection')
 const app = express()
 app.use(express.json())
@@ -8,36 +11,71 @@ app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
     res.send("welcome")
 })
-
-app.post('/api/register',async (req,res,next)=>{
+app.post('/api/register', async (req, res, next) => {
     try {
-     const {fullname,email,password}=req.body   
-     if (!fullname||!email||!password) 
-     {
-      res.status(400).send("Fill all fields")  
-     }
-     else
-     {
-    const alreadyexist=await Users.findOne({email})
+        const { fullname, email, password } = req.body
+        if (!fullname || !email || !password) {
+            res.status(400).send("Fill all fields")
+        }
+        else {
+            const alreadyexist = await Users.findOne({ email })
 
-    if (alreadyexist) 
-    {
-        res.status(400).send("User Already Exists")    
-    }
-    else{
-        const newuser= new Users({fullname,email})
-        bycrypt.hash(password,10,(err,hashedpassword)=>{
-            newuser.set('password',hashedpassword)
-            newuser.save();
-            next()
-        })
-        return res.status(200).send("User Registered Successfully")
-    }
-     }
+            if (alreadyexist) {
+                res.status(400).send("User Already Exists")
+            }
+            else {
+                const newuser = new Users({ fullname, email })
+                bycrypt.hash(password, 10, (err, hashedpassword) => {
+                    newuser.set('password', hashedpassword)
+                    newuser.save();
+                    next()
+                })
+                return res.status(200).send("User Registered Successfully")
+            }
+        }
     } catch (error) {
-        
+        console.error(error)
     }
+})
+app.post('/api/login', async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+        if (!email || !password) {
+            res.status(400).send("Fill all fields")
+        }
+        else {
+            const user = await Users.findOne({ email })
+            if (!user) {
+                res.status(400).send("User or password is wrong")
+            }
+            else {
+                const validateuser = await bycrypt.compare(password, user.password)
+                if (!validateuser) {
+                    res.status(400).send("User or password is wrong")
+                }
+                else {
+                    const payload = {
+                        userId: user._id,
+                        userEmail: user.email
+                    }
+                    const jwtkey = 'this_is_a_secret_key_which_doesnt_need_to_be_exposed'
+                    jwt.sign(payload, jwtkey, {
+                        expiresIn: 84600
+                    }, async (err,token) => {
+                        await Users.updateOne({ _id: user.id, }, {
+                            $set: { token }
+                        })
+                        user.save()
+                        next()
+                    })
+                }
+                res.status(200).json({ user:{fullname:user.fullname,email:user.email},token:user.token})
+            }
+        }
 
+    } catch (error) {
+        console.error(error)
+    }
 })
 app.listen(8000, () => {
     console.log('Listining');
